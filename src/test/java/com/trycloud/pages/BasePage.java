bb14ef6a8b5d368e24685b290e4e5e3dc315ece4
igorkidd <igor22kidd@gmail.com>
2025-06-02 14:48:18 -0400

    Adding needed elements to the base page

--- src/test/java/com/trycloud/pages/BasePage.java
+++ src/test/java/com/trycloud/pages/BasePage.java
@@ -1,4 +1,16 @@
 package com.trycloud.pages;
 
+import com.trycloud.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
 public class BasePage {
+    public BasePage() {
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
+    @FindBy(xpath = "//div[@id='settings']")
+    public WebElement userIcon;
+    @FindBy(xpath = "//a[@href='/index.php/settings/user']")
+    public WebElement userSettings;
 }
