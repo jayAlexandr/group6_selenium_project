8e3d378f6d75985859321dc209aa846914004949
Nicholas <nick.makmak@gmail.com>
2025-06-02 19:37:15 -0400

    WebElement filesModuleButton added

--- src/test/java/com/trycloud/pages/BasePage.java
+++ src/test/java/com/trycloud/pages/BasePage.java
@@ -1,4 +1,17 @@
 package com.trycloud.pages;
 
+import com.trycloud.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
 public class BasePage {
+
+    public BasePage(){
+        PageFactory.initElements(Driver.getDriver(),this);
+    }
+
+    @FindBy (xpath = "//li[@data-id='files']")
+    public WebElement filesModuleButton;
+
 }
