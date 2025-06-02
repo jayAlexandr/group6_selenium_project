90af92c4415c5a214493d8f5cdd24ac2d60d4d7f
Igor Risk <109050609+mynameiskidd@users.noreply.github.com>
2025-06-02 19:52:23 -0400

    Finishing first test scenario - User can see at least following titles inside personal info under Profile Settings page =>Full name/Email/Phone Number

--- src/test/java/com/trycloud/step_definitions/EnterEditData_StepDefinitions.java
+++ src/test/java/com/trycloud/step_definitions/EnterEditData_StepDefinitions.java
@@ -2,11 +2,15 @@ package com.trycloud.step_definitions;
 
 import com.trycloud.pages.BasePage;
 import com.trycloud.pages.LoginPage;
+import com.trycloud.pages.SettingsPage;
 import com.trycloud.utilities.BrowserUtils;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.junit.Assert;
 
 public class EnterEditData_StepDefinitions {
+    // creating an object for setting page globally so I can use it later in different steps
+    SettingsPage settingsPage = new SettingsPage();
 
     @When("user is logged in the app and on the settings page")
     public void user_is_logged_in_the_app_and_on_the_settings_page() {
@@ -23,10 +27,12 @@ public class EnterEditData_StepDefinitions {
         basePage.userIcon.click();
         basePage.userSettings.click();
     }
-    //TODO this step
+
     @Then("user sees Full name, Email, Phone Number displayed")
     public void user_sees_full_name_email_phone_number_displayed() {
-
-
+        //using assert checking if web needed web elements are displayed.
+        Assert.assertTrue(settingsPage.fullNameLabel.isDisplayed());
+        Assert.assertTrue(settingsPage.emailLabel.isDisplayed());
+        Assert.assertTrue(settingsPage.phoneNumberLabel.isDisplayed());
     }
 }
