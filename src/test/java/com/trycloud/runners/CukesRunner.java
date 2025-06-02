9aba6a87a75a8598bf5ffa83042bfec49dbdf8a1
igorkidd <igor22kidd@gmail.com>
2025-06-02 14:48:44 -0400

    Setting up Cukes runner for my feature file

--- src/test/java/com/trycloud/runners/CukesRunner.java
+++ src/test/java/com/trycloud/runners/CukesRunner.java
@@ -14,7 +14,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",
         glue = "com/trycloud/step_definitions",
         dryRun = false,
-        tags = "@wip",
+        tags = "@us-15",
         publish = true //generating a report with public link
 )
 public class CukesRunner {}
