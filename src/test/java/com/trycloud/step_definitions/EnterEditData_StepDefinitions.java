3d03ccb1f1c4c1b4aa57187905f93cd534f078bd
igorkidd <igor22kidd@gmail.com>
2025-06-03 14:11:10 -0400

    Adding TC3

--- src/test/java/com/trycloud/step_definitions/EnterEditData_StepDefinitions.java
+++ src/test/java/com/trycloud/step_definitions/EnterEditData_StepDefinitions.java
@@ -4,14 +4,17 @@ import com.trycloud.pages.BasePage;
 import com.trycloud.pages.LoginPage;
 import com.trycloud.pages.SettingsPage;
 import com.trycloud.utilities.BrowserUtils;
+import io.cucumber.java.bs.A;
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.junit.Assert;
+import org.openqa.selenium.Keys;
 
 public class EnterEditData_StepDefinitions {
     // creating an object for setting page globally so I can use it later in different steps
     SettingsPage settingsPage = new SettingsPage();
-
+    //TC1
     @When("user is logged in the app and on the settings page")
     public void user_is_logged_in_the_app_and_on_the_settings_page() {
         //Using login page to create an object
@@ -35,4 +38,35 @@ public class EnterEditData_StepDefinitions {
         Assert.assertTrue(settingsPage.emailLabel.isDisplayed());
         Assert.assertTrue(settingsPage.phoneNumberLabel.isDisplayed());
     }
+    //TC2
+
+    @When("user enters a new name {string} into the Full name field and presses Enter")
+    public void user_enters_a_new_name_into_the_full_name_field_and_presses_enter(String name) {
+        settingsPage.fullNameInput.clear();
+        settingsPage.fullNameInput.sendKeys(name, Keys.ENTER);
+    }
+    @Then("user sees a new name {string} is saved in the Full name field")
+    public void user_sees_a_new_name_is_saved_in_the_full_name_field(String expectedName) {
+        Assert.assertTrue(settingsPage.fullNameInput.getDomAttribute("value").contains(expectedName));
+    }
+    //TC3
+ //TODO this TC3
+    @When("user clicks on the options icon next to Phone number label")
+    public void user_clicks_on_the_options_icon_next_to_phone_number_label() {
+        settingsPage.phoneSettingsIcon.click();
+
+
+    }
+    @When("user selects private option")
+    public void user_selects_private_option() {
+        settingsPage.privatePhoneSelection.click();
+    }
+    @Then("user sees phone number settings changed to private")
+    public void user_sees_phone_number_settings_changed_to_private() {
+        System.out.println("settingsPage.privatePhoneSelection.getDomAttribute(\"class\") = " + settingsPage.privatePhoneSelection.getDomAttribute("class"));
+
+    }
+
+
 }
+
