321104dcb252e31e3f93ed05083527cf78a24085
sunisaservais <sunisaservais@gmaill.com>
2025-06-02 21:18:06 -0400

    StepDefinitions file for US-03 is created

---
+++ src/test/java/com/trycloud/step_definitions/US_03_Dashboard_StepDefinitions.java
@@ -0,0 +1,47 @@
+package com.trycloud.step_definitions;
+
+import com.trycloud.pages.DashboardPage;
+import com.trycloud.utilities.BrowserUtils;
+import io.cucumber.java.en.Then;
+import org.junit.Assert;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
+
+import static com.trycloud.utilities.BrowserUtils.*;
+
+public class US_03_Dashboard_StepDefinitions {
+
+    DashboardPage dashboardPage = new DashboardPage();
+
+    @Then("user should be able to see following modules")
+    public void user_user_should_be_able_to_see_following_modules(List<String> expectedModuleNames) {
+        List<String> actualModuleNames = dashboardPage.moduleName(dashboardPage.modulesName, "aria-label");
+        Assert.assertEquals(expectedModuleNames, actualModuleNames);
+    }
+
+    @Then("user click on user profile menu")
+    public void user_click_on_user_profile_menu() {
+        dashboardPage.profileMenu.click();
+    }
+
+    @Then("user can see username")
+    public void user_can_see_username() {
+        Assert.assertTrue(dashboardPage.userName.isDisplayed());
+    }
+
+    @Then("user click on Customize button")
+    public void user_click_on_customize_button() {
+        dashboardPage.customizeButton.click();
+    }
+
+    @Then("user can see status widgets")
+    public void user_can_see_status_widgets() {
+        dashboardPage.verifyWidgets();
+    }
+
+    @Then("user can select any of them")
+    public void user_can_select_any_of_them() {
+
+    }
+}
