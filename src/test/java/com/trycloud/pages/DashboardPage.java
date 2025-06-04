bd1745d565417fd964ad8ff92cc90e6618e341ec
sunisaservais <sunisaservais@gmaill.com>
2025-06-04 11:14:52 -0400

    DashboardPage: updated

--- src/test/java/com/trycloud/pages/DashboardPage.java
+++ src/test/java/com/trycloud/pages/DashboardPage.java
@@ -1,6 +1,5 @@
 package com.trycloud.pages;
 
-import com.trycloud.utilities.BrowserUtils;
 import com.trycloud.utilities.Driver;
 import org.junit.Assert;
 import org.openqa.selenium.JavascriptExecutor;
@@ -29,8 +28,15 @@ public class DashboardPage extends BasePage {
     @FindBy(xpath = "//button[@tabindex='0']")
     public List<WebElement> background;
 
-    @FindBy(className = "oc-dialog-close")
-    public WebElement exit;
+    @FindBy(xpath = "//button[@class='background filepicker']")
+    public WebElement pickFromFile;
+
+    @FindBy(xpath = "//td[@class='filename']")
+    public WebElement image;
+
+    @FindBy(xpath = "//button[@class='primary']")
+    public WebElement choose;
+
 
     public List<String> moduleNames(List<WebElement> webElement) {
         List<String> names = new ArrayList<>();
@@ -41,22 +47,23 @@ public class DashboardPage extends BasePage {
         return names;
     }
 
-    public void verifyWidgetsIsDisplayed() {
+    public void verifyWidgetsAreDisplayed() {
         for (WebElement each : widgetCheckbox) {
-            Assert.assertTrue(each.isSelected());
+            Assert.assertTrue("Widget checkbox is not enabled: " + each.getAttribute("id"), each.isEnabled());
         }
     }
 
-    public void checkboxesSelected() {
+    public void selectAllWidgets() {
         for (WebElement eachCheckbox : widgetCheckbox) {
             if (eachCheckbox.isSelected()) {
                 continue;
             }
-            BrowserUtils.clickWithJS(eachCheckbox);
+            clickWithJS(eachCheckbox);
+            Assert.assertTrue(eachCheckbox.isSelected());
         }
     }
 
-    public void verifyBackgroundIsDisplay() {
+    public void verifyBackgroundsAreDisplay() {
         JavascriptExecutor js = (JavascriptExecutor) Driver.getDriver();
         js.executeScript("window.scrollBy(0, 200);");
         for (WebElement eachImage : background) {
@@ -71,7 +78,18 @@ public class DashboardPage extends BasePage {
                 continue;
             }
             clickWithJS(eachImage);
+            String actualBackground = eachImage.getAttribute("class");
+            String expectedBackground = "background";
+            System.out.println(actualBackground);
+            Assert.assertTrue("Background not selected: " + text, actualBackground.contains(expectedBackground));
         }
     }
-}
 
+    public void verifyChooseBGFromFile() {
+        customizeButton.click();
+        pickFromFile.click();
+        image.click();
+        choose.click();
+        Assert.assertTrue(image.isDisplayed());
+    }
+}
