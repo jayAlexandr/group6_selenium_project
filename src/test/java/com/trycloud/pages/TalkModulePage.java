1f932749eca59db1c5886f2f27e1ee4789ae902f
sunisaservais <sunisaservais@gmaill.com>
2025-06-12 15:34:26 -0400

    Talk Module Page: Done

--- src/test/java/com/trycloud/pages/TalkModulePage.java
+++ src/test/java/com/trycloud/pages/TalkModulePage.java
@@ -5,8 +5,7 @@ import org.openqa.selenium.support.FindBy;
 
 import java.util.List;
 
-public class TalkModulePage extends BasePage{
-
+public class TalkModulePage extends BasePage {
 
     @FindBy(xpath = "//button[@aria-label='Create a new group conversation']")
     public WebElement newConversationButton;
@@ -26,12 +25,45 @@ public class TalkModulePage extends BasePage{
     @FindBy(xpath = "//span[@class='acli__content__line-one__title']")
     public List<WebElement> conversationList;
 
-    @FindBy(xpath = "//button[@rel='noreferrer noopener']")
-    public WebElement addParticipantsToGroupButton;
-
     @FindBy(xpath = "//input[@placeholder='Add participants to the conversation']")
     public WebElement addParticipantInputBox;
 
     @FindBy(xpath = "//li[contains(@class,'participant-row')]")
     public List<WebElement> participantsListAfterAdded;
+
+    @FindBy(xpath = "//li[@class='app-navigation-hint']")
+    public WebElement noSearchResult;
+
+    @FindBy(xpath = "//span[@class='icon icon-start-call']")
+    public WebElement callButton;
+
+    @FindBy(xpath = "//span[@class='acli__content__line-two__subtitle'][1]")
+    public WebElement youStartedCallMessage;
+
+    @FindBy(xpath = "//button[@class='top-bar__button error top-bar__button']")
+    public WebElement leaveCallButton;
+
+    @FindBy(xpath = "//a[@aria-controls='app-navigation-vue']")
+    public WebElement threeBars;
+
+    @FindBy(xpath = "//span[@class='acli__content__line-one__title'][1]")
+    public WebElement groupName;
+
+    @FindBy(xpath = "//button[@aria-label='Conversation settings']")
+    public List<WebElement> threeVerticalDots;
+
+    @FindBy(xpath = "//span[contains(text(),'Delete conversation')]")
+    public WebElement deleteButton;
+
+    @FindBy(xpath = "//button[contains(text(),'No')]")
+    public WebElement declinedButton;
+
+    @FindBy(xpath = "//button[@class='primary']")
+    public WebElement confirmDelete;
+
+    @FindBy(xpath = "//input[@placeholder='Search conversations or users']")
+    public WebElement searchConversationInputBox;
+
+    @FindBy(xpath = "//li[@class='app-navigation-hint']")
+    public WebElement noMatchesMessage;
 }
