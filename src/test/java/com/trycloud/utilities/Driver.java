5c7ea07a79f4478fc439498b888f4db95cdf396c
sunisaservais <sunisaservais@gmaill.com>
2025-06-13 09:30:54 -0400

    Chrome option added for launching Chrome browser.

--- src/test/java/com/trycloud/utilities/Driver.java
+++ src/test/java/com/trycloud/utilities/Driver.java
@@ -1,16 +1,21 @@
 
 package com.trycloud.utilities;
 
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 import java.time.Duration;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Driver {
 
     //create a private constructor to remove access to this object
-    private Driver(){}
+    private Driver() {
+    }
 
     /*
     We make the WebDriver private, because we want to close access from outside the class.
@@ -24,9 +29,9 @@ public class Driver {
     Create a re-usable utility method which will return the same driver instance once we call it.
     - If an instance doesn't exist, it will create first, and then it will always return same instance.
      */
-    public static WebDriver getDriver(){
+    public static WebDriver getDriver() {
 
-        if(driverPool.get() == null){
+        if (driverPool.get() == null) {
 
             /*
             We will read our browserType from configuration.properties file.
@@ -38,10 +43,28 @@ public class Driver {
             Depending on the browserType returned from the configuration.properties
             switch statement will determine the "case", and open the matching browser.
              */
-            switch (browserType){
+            switch (browserType) {
                 case "chrome":
                     //WebDriverManager.chromedriver().setup();
-                    driverPool.set(new ChromeDriver());
+                    ChromeOptions options = new ChromeOptions(); //added for me
+                    options.addArguments("force-device-scale-factor=1.2"); //added screen size
+
+                    // Automatically allow camera and microphone
+                    Map<String, Object> prefs = new HashMap<>();
+                    prefs.put("profile.default_content_setting_values.media_stream_camera", 1);
+                    prefs.put("profile.default_content_setting_values.media_stream_mic", 1);
+                    prefs.put("profile.default_content_setting_values.geolocation", 1);
+                    prefs.put("profile.default_content_setting_values.notifications", 1);
+                    options.setExperimentalOption("prefs", prefs);
+
+                    // Disable infobars and popups
+                    options.addArguments("--use-fake-ui-for-media-stream"); // auto-accept camera/mic
+                    options.addArguments("--use-fake-device-for-media-stream"); // optional: use dummy stream
+                    options.addArguments("--disable-infobars");
+                    options.addArguments("--disable-popup-blocking");
+                    options.addArguments("--disable-notifications");
+
+                    driverPool.set(new ChromeDriver(options));
                     driverPool.get().manage().window().maximize();
                     driverPool.get().manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
                     break;
@@ -62,8 +85,8 @@ public class Driver {
     /*
     Create a new Driver.closeDriver(); it will use .quit() method to quit browsers, and then set the driver value back to null.
      */
-    public static void closeDriver(){
-        if (driverPool.get()!=null){
+    public static void closeDriver() {
+        if (driverPool.get() != null) {
             /*
             This line will terminate the currently existing driver completely. It will not exist going forward.
              */
